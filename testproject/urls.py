6d6e8d323ba25e3751146df4d47557e30c597655
Latheef <latheefvkpadi@gmail.com>
2015-02-20 12:24:16 +0530

    added project

---
+++ testproject/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'testapp.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                 {'document_root': settings.MEDIA_ROOT}),
+)
